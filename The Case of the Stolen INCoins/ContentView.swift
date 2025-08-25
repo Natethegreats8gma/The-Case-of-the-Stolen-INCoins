30cdb6a26cc460d16dcb2a070a442b935c898c85
FlatDurian13 <tim_mao_feng_aidan@s2025.ssts.edu.sg>
2025-08-25 16:52:27 +0800

    Initial Commit

---
+++ The Case of the Stolen INCoins/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Case of the Stolen INCoins
+//
+//  Created by SST Aidan on 25/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Case of the Stolen INCoins/The_Case_of_the_Stolen_INCoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Case_of_the_Stolen_INCoinsApp.swift
+//  The Case of the Stolen INCoins
+//
+//  Created by SST Aidan on 25/8/25.
+//
+
+import SwiftUI
+
+@main
+struct The_Case_of_the_Stolen_INCoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
